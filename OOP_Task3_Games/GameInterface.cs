b22b3caa64b56fe7775f4a39907d19fc64061107
Julia <kuznetsovajulia@gmail.com>
2024-05-21 00:47:01 +0300

    Добавьте файлы проекта.

---
+++ OOP_Task3_Games/GameInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task3_Games
+{
+    public interface GameInterface
+    {
+        void startGame();
+        string getGameName();
+        bool isWin();
+    }
+}
---
+++ OOP_Task3_Games/GamesApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task3_Games
+{
+    public class GamesApp
+    {
+        private static List<Gamer> gamers; 
+        private static List<GameInterface> games;
+        public GamesApp()
+        {
+            gamers = new List<Gamer>();
+        }
+        public bool auth(string username, string password)
+        {
+            return true;
+        }
+        public bool registr(string username, string password)
+        {
+            gamers.Add(new Gamer(username));
+            return true;
+        }
+        public void addGame(GameInterface game)
+        {
+            games.Add(game);
+        }
+        public void beginGamesApp()
+        {
+            auth("admin", "admin");
+        }
+    }
+}
