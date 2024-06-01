04c3c7c1d9b8f655f3eb020aecfe69b7e8b95423
Julia <kuznetsovajulia@gmail.com>
2024-06-01 21:49:50 +0300

    доработан графический интерфейс

--- OOP_Task3_Games/MainWindow.xaml.cs
+++ OOP_Task3_Games/MainWindow.xaml.cs
@@ -24,5 +24,32 @@ namespace OOP_Task3_Games
         {
             InitializeComponent();
         }
+
+        
+        private void button_enter_Click(object sender, RoutedEventArgs e)
+        {
+            textBox_greeting.Text = "Hello, Gamer!";
+        }
+
+        private void button_play_Click(object sender, RoutedEventArgs e)
+        {
+            GamesApp gamesApp = new GamesApp();
+            gamesApp.beginGamesApp();
+
+            listBox_list_games.Items.Add("Крестики-нолики");
+            listBox_list_games.Items.Add("Виселица");
+            listBox_list_games.Items.Add("Пасьянс");
+            listBox_list_games.Items.Add("Змейка");
+
+            listBox_queue_games.Items.Add("Виселица");
+            listBox_queue_games.Items.Add("Пасьянс");
+            listBox_queue_games.Items.Add("Крестики-нолики");
+            listBox_queue_games.Items.Add("Змейка");
+
+            listBox_rating.Items.Add("Julia");
+            listBox_rating.Items.Add("Kate");
+            listBox_rating.Items.Add("Sasha");
+            listBox_rating.Items.Add("Sam");
+        }
     }
 }
--- OOP_Task3_Games/Program.cs
+++ OOP_Task3_Games/Program.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOP_Task3_Games
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, Gamer!");
-            GamesApp gamesApp = new GamesApp();
-            gamesApp.beginGamesApp();
-        }
-    }
-}
+//namespace OOP_Task3_Games
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, Gamer!");
+//            GamesApp gamesApp = new GamesApp();
+//            gamesApp.beginGamesApp();
+//        }
+//    }
+//}
